c063b31be50c6d9c90eee2fb92b3c3cdbbc1114f
Bartosz Elert <elert.bartosz@gmail.com>
2024-05-18 21:12:07 +0200

    added observer pattern example

---
+++ src/ObserverPattern/pokedex.data.ts
@@ -0,0 +1,124 @@
+import type { PokedexData } from "./Pokedex";
+
+export const pokemons: PokedexData[] = [
+  {
+    name: "Bulbasaur",
+    category: "Seed",
+    weight: 6.9,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Ivysaur",
+    category: "Seed",
+    weight: 13.0,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Venusaur",
+    category: "Seed",
+    weight: 100.0,
+    abilities: ["Overgrow", "Chlorophyll"],
+  },
+  {
+    name: "Charmander",
+    category: "Lizard",
+    weight: 8.5,
+    abilities: ["Blaze", "Solar Power"],
+  },
+  {
+    name: "Charmeleon",
+    category: "Flame",
+    weight: 19.0,
+    abilities: ["Blaze", "Solar Power"],
+  },
+  {
+    name: "Charizard",
+    category: "Flame",
+    weight: 90.5,
+    abilities: ["Blaze", "Solar Power"],
+  },
+  {
+    name: "Squirtle",
+    category: "Tiny Turtle",
+    weight: 9.0,
+    abilities: ["Torrent", "Rain Dish"],
+  },
+  {
+    name: "Wartortle",
+    category: "Turtle",
+    weight: 22.5,
+    abilities: ["Torrent", "Rain Dish"],
+  },
+  {
+    name: "Blastoise",
+    category: "Shellfish",
+    weight: 85.5,
+    abilities: ["Torrent", "Rain Dish"],
+  },
+  {
+    name: "Caterpie",
+    category: "Worm",
+    weight: 2.9,
+    abilities: ["Shield Dust", "Run Away"],
+  },
+  {
+    name: "Metapod",
+    category: "Cocoon",
+    weight: 9.9,
+    abilities: ["Shed Skin"],
+  },
+  {
+    name: "Butterfree",
+    category: "Butterfly",
+    weight: 32.0,
+    abilities: ["Compound Eyes", "Tinted Lens"],
+  },
+  {
+    name: "Weedle",
+    category: "Hairy Bug",
+    weight: 3.2,
+    abilities: ["Shield Dust", "Run Away"],
+  },
+  {
+    name: "Kakuna",
+    category: "Cocoon",
+    weight: 10.0,
+    abilities: ["Shed Skin"],
+  },
+  {
+    name: "Beedrill",
+    category: "Poison Bee",
+    weight: 29.5,
+    abilities: ["Swarm", "Sniper"],
+  },
+  {
+    name: "Pidgey",
+    category: "Tiny Bird",
+    weight: 1.8,
+    abilities: ["Keen Eye", "Tangled Feet", "Big Pecks"],
+  },
+  {
+    name: "Pidgeotto",
+    category: "Bird",
+    weight: 30.0,
+    abilities: ["Keen Eye", "Tangled Feet", "Big Pecks"],
+  },
+  {
+    name: "Pidgeot",
+    category: "Bird",
+    weight: 39.5,
+    abilities: ["Keen Eye", "Tangled Feet", "Big Pecks"],
+  },
+  {
+    name: "Rattata",
+    category: "Mouse",
+    weight: 3.5,
+    abilities: ["Run Away", "Guts", "Hustle"],
+  },
+  {
+    name: "Raticate",
+    category: "Mouse",
+    weight: 18.5,
+    abilities: ["Run Away", "Guts", "Hustle"],
+  },
+];
